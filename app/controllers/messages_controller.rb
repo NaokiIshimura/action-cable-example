3b219829e410322f6eea0ecc2b77f01a7e15a11a
Naoki Ishimura <mirontest1460@gmail.com>
2018-04-24 21:07:59 +0900

    #9 Messages appear only in proper chatrooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,19 @@
 class MessagesController < ApplicationController
-
+  
   def create
     message = Message.new(message_params)
     message.user = current_user
     if message.save
-      ActionCable.server.broadcast 'messages',
-        message: message.content,
-        user: message.user.username
+      ActionCable.server.broadcast "messages_#{message.chatroom_id}_channel",
+                                   message: message.content,
+                                   user: message.user.username
       head :ok
     end
   end
-
+  
   private
-
-    def message_params
-      params.require(:message).permit(:content, :chatroom_id)
-    end
+  
+  def message_params
+    params.require(:message).permit(:content, :chatroom_id)
+  end
 end
